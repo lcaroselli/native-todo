075c4997a29632c384efbaa24041dfd2cfde66a8
Laura Caroselli <lcaroselli@outlook.com>
2017-10-21 16:42:55 -0600

    Display mock list data

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ListView, StyleSheet, Text, View } from 'react-native';
 
 import Title from './components/Title/Title';
 import Footer from './components/Footer/Footer';
 
+const rowData = [
+  {id: 0, text: 'Walk Dog'},
+  {id: 1, text: 'Clean Kitchen'},
+  {id: 2, text: 'Grocery Shopping'},
+  {id: 3, text: 'Finish React Native Tutorial'},
+  {id: 4, text: 'Schedule Coffee with Jeanette'}
+]
+
+const rowHasChanged = (row1, row2) => row1.id !== row2.id
+const dataSource = new ListView.DataSource({ rowHasChanged })
+
+
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      listData: dataSource.cloneWithRows(rowData)
+    }
+  }
+
+  renderRow = (data) => {
+    return (
+      <Text>
+      { data.text }
+      </Text>
+    )
+  }
   render() {
     return (
       <View style={ styles.container }>
         <Title></Title>
+        <ListView
+          style={ styles.list }
+          dataSource={ this.state.listData }
+          renderRow={ this.renderRow }/>
         <Footer></Footer>
       </View>
     );
@@ -19,5 +49,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  list: {
+    backgroundColor: 'white'
   },
 });
